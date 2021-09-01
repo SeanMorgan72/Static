f7da95764bde9ac128e8fe8619a3c2564d514eeb
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-09-01 15:11:34 -0500

    completed static project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Fahrenheit degree temp you need to convert: ");
+            var degree = double.Parse(Console.ReadLine());
+
+            var degrees = TempConverter.FahrenheitToCelsius(degree);
+
+            Console.WriteLine("Enter the Celcius temp you need to convert: ");
+            var celcius = double.Parse(Console.ReadLine());
+
+            var change = TempConverter.CelsiusToFahrenheit(celcius);
+
+            Console.WriteLine($"Fahrenheit: {degree} degrees in Celcius is: {degrees}.");
+            Console.WriteLine("");
+            Console.WriteLine($"Celcius: {celcius} degrees in Fahrenheit is: {change}.");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Static
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double temp)
+        {
+            return Math.Round((temp - 32.00) * 5 /9);
+        }
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            return Math.Round ((temp *9) / 5 + 32.00);
+        }
+
+    }
+}
